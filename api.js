fbcf02e0c6c33f3e29eb4a6412435789d1321a4a
COURDIER <evann.courdier@mines-paristech.fr>
2015-05-22 01:13:54 +0200

    Emulated api - first commit

---
+++ api.js
@@ -0,0 +1,34 @@
+var api = {};
+
+api.dataPoints = d3.range(0,25,0.1).map(function(d) {return [d, Math.cos(d)*3];});
+
+api.getConf;
+
+api.getDataDimensions = function () {
+	var dimensions = {};
+	dimensions.xmin = d3.min(api.dataPoints, d(0));
+	dimensions.xmax = d3.max(api.dataPoints, d(0));
+	dimensions.ymin = d3.min(api.dataPoints, d(1));
+	dimensions.ymax = d3.max(api.dataPoints, d(1));
+	return dimensions;
+};
+
+//Not perfect ... need the case x = xmax
+api.getData = function (limit) {
+	var filteredData = api.dataPoints.filter(function (d) { return d[0] >= limit.xmin && d[0] < limit.xmax && d[1] >= limit.ymin && d[1] < limit.ymax;})
+	return filteredData;
+};
+
+api.getConf = function () {
+	var c = {};
+	c.columns = 4;
+	c.rows = 2;
+	c.margin = {
+			top : 20,
+			bottom : 20,
+			left : 60,
+			right : 60
+	};
+	
+	return c;
+};
