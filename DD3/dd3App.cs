1d11385752749bcdcf5221377528eec482049d6f
COURDIER <evann.courdier@mines-paristech.fr>
2015-08-03 17:58:58 +0800

    Adaptation to GDO

---
+++ DD3/dd3App.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using GDO.Core;
+using GDO.Utility;
+
+namespace GDO.Apps.DD3
+{
+    public class dd3App : IAppInstance
+    {
+        public int Id { get; set; }
+        public Section Section { get; set; }
+        public AppConfiguration Configuration { get; set; }
+
+        public void init(int instanceId, Section section, AppConfiguration configuration)
+        {
+            this.Id = instanceId;
+            this.Section = section;
+            this.Configuration = configuration;
+        }
+
+        private ConcurrentDictionary<string, BrowserInfo> browserList = new ConcurrentDictionary<string, BrowserInfo>();
+        private ConcurrentDictionary<string, bool> syncList = new ConcurrentDictionary<string, bool>();
+        private static dd3AppHub hub;
+        private readonly object _locker = new Object();
+
+        public void newClient(string cid, BrowserInfo b)
+        {
+            lock (_locker)
+            {
+                browserList.TryAdd(cid, new BrowserInfo(cid, b.browserNum, b.peerId, b.col, b.row, b.height, b.width));
+                if (browserList.Count == Section.NumNodes)
+                {
+                    broadcastConfiguration();
+                }
+            }
+        }
+
+        private void broadcastConfiguration () {
+            BrowserBroadcastInfo[] browserInfos = new BrowserBroadcastInfo[browserList.Count];
+            int i = 0;
+
+            foreach (var item in browserList.Values)
+            {
+                browserInfos[i] = new BrowserBroadcastInfo(item.browserNum, item.peerId, item.col, item.row);
+                i++;
+            }
+
+            String browserInfoJson = Newtonsoft.Json.JsonConvert.SerializeObject(browserInfos);
+            hub.broadcastConfiguration(Id, browserInfoJson);
+        }
+
+        public void synchronize(string cid)
+        {
+            syncList.AddOrUpdate(cid, true, (key, value) => true);
+            if (syncList.Count == Section.NumNodes)
+            {
+                hub.broadcastSynchronize(Id);
+                syncList.Clear();
+            }
+        }
+    }
+
+    public class BrowserBroadcastInfo
+    {
+        public BrowserBroadcastInfo(string browserNum, string peerId, string col, string row)
+        {
+            this.browserNum = browserNum;
+            this.peerId = peerId;
+            this.col = col;
+            this.row = row;
+        }
+
+        public string browserNum { get; set; }
+        public string peerId { get; set; }
+        public string col { get; set; }
+        public string row { get; set; }
+    }
+
+    public class BrowserInfo
+    {
+        public BrowserInfo(string connectionId, string browserNum, string peerId, string col, string row, string height, string width)
+        {
+            this.connectionId = connectionId;
+            this.browserNum = browserNum;
+            this.peerId = peerId;
+            this.col = col;
+            this.row = row;
+            this.height = height;
+            this.width = width;
+        }
+
+        public string connectionId { get; set; }
+        public string browserNum { get; set; }
+        public string peerId { get; set; }
+        public string col { get; set; }
+        public string row { get; set; }
+        public string height { get; set; }
+        public string width { get; set; }
+    }
+}
+
+//yky112358
---
+++ DD3/dd3AppHub.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Concurrent;
+using GDO.Core;
+using GDO.Utility;
+
+namespace GDO.Apps.DD3
+{
+    public class dd3AppHub : Hub, IAppHub
+    {
+
+        private readonly ConcurrentDictionary<int, IAppInstance> instances = Cave.Apps["dd3App"].Instances;
+        private readonly object _locker = new Object();
+
+        public string Name { get; set; }
+        public int P2PMode { get; set; }
+        public Type InstanceType { get; set; }
+
+        public dd3AppHub () {
+            this.Name = "dd3";
+            this.P2PMode = (int) Cave.P2PModes.Neighbours;
+            this.InstanceType = new dd3App().GetType();
+        }
+
+        public void JoinGroup(int instanceId)
+        { 
+            Groups.Add(Context.ConnectionId, "" + instanceId);
+        }
+        public void ExitGroup(int instanceId)
+        {
+            Groups.Remove(Context.ConnectionId, "" + instanceId);
+        }
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected(Boolean b)
+        {
+            return base.OnDisconnected(b);
+        }
+
+        public void updateInformation (int instanceId, BrowserInfo b)
+        {
+           ((dd3App) instances[instanceId]).newClient(Context.ConnectionId, b);
+            JoinGroup(instanceId);
+        }
+
+        public void synchronize(int instanceId)
+        {
+            ((dd3App) instances[instanceId]).synchronize(Context.ConnectionId);
+        }
+
+        public void broadcastSynchronize(int instanceId)
+        {
+            Clients.Group("" + instanceId).synchronize();
+        }
+
+        public void broadcastConfiguration(int instanceId, string info)
+        {
+            Clients.Group("" + instanceId).receiveConfiguration(info, instanceId);
+        }
+    }
+}
